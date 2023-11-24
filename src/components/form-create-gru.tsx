e9288d7e0e31b20fe9162611278a6ba85a14e3f8
Enrique Santos <santosenrique869@gmail.com>
2023-11-24 18:39:09 -0300

    feat: add Card component to form-create-gru.tsx

--- src/components/form-create-gru.tsx
+++ src/components/form-create-gru.tsx
@@ -12,6 +12,7 @@ import { LoadingIndicator } from "@/components/loading-indicator";
 import { useGenerateGRU } from "@/hooks/useGenerateGRU";
 
 import { validateCPF } from "@/helpers/validate-cpf";
+import { Card, CardContent } from "./ui/card";
 
 const createGRUSchema = z.object({
   name: z.string().trim().min(5, "Nome deve ter no mínimo 5 caracteres"),
@@ -60,86 +61,90 @@ export function FormCreateGRU({ type }: FormCreateGRUProps) {
   const { mutation, loading } = useGenerateGRU();
 
   return (
-    <form className="mt-6 flex flex-col gap-4" onSubmit={onSubmit}>
-      <div className="space-y-2">
-        <Label htmlFor="name">Nome completo *</Label>
-        <Input
-          {...register("name")}
-          id="name"
-          type="text"
-          placeholder="Digite seu nome"
-        />
-        {errors.name && (
-          <span className="text-sm text-destructive">
-            {errors.name.message}
-          </span>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="name">CPF *</Label>
-        <Controller
-          name="document"
-          control={control}
-          render={({ field }) => (
+    <Card className="mt-6">
+      <CardContent className="p-6">
+        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+          <div className="space-y-2">
+            <Label htmlFor="name">Nome completo *</Label>
             <Input
-              {...field}
+              {...register("name")}
               id="name"
               type="text"
-              placeholder="000.000.000-00"
-              value={field.value}
-              onChange={(e) => field.onChange(makeMaskCpf(e.target.value))}
+              placeholder="Digite seu nome"
             />
-          )}
-        />
-        {errors.document && (
-          <span className="text-sm text-destructive">
-            {errors.document.message}
-          </span>
-        )}
-      </div>
+            {errors.name && (
+              <span className="text-sm text-destructive">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="name">CPF *</Label>
+            <Controller
+              name="document"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  id="name"
+                  type="text"
+                  placeholder="000.000.000-00"
+                  value={field.value}
+                  onChange={(e) => field.onChange(makeMaskCpf(e.target.value))}
+                />
+              )}
+            />
+            {errors.document && (
+              <span className="text-sm text-destructive">
+                {errors.document.message}
+              </span>
+            )}
+          </div>
 
-      <div className="space-y-2">
-        <Label htmlFor="name">Valor *</Label>
-        <Input {...register("value")} id="name" type="number" />
-        {errors.value && (
-          <span className="text-sm text-destructive">
-            {errors.value.message}
-          </span>
-        )}
-      </div>
+          <div className="space-y-2">
+            <Label htmlFor="name">Valor *</Label>
+            <Input {...register("value")} id="name" type="number" />
+            {errors.value && (
+              <span className="text-sm text-destructive">
+                {errors.value.message}
+              </span>
+            )}
+          </div>
 
-      <Button
-        type="submit"
-        size="lg"
-        aria-label="Gerar GRU"
-        className="mt-3 bg-[#06956A] font-medium hover:bg-[#06945A]"
-        aria-disabled={loading}
-        disabled={loading}
-      >
-        {loading ? (
-          <LoadingIndicator />
-        ) : (
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-4 w-4"
-            >
-              <polyline points="6 9 6 2 18 2 18 9" />
-              <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
-              <rect width="12" height="8" x="6" y="14" />
-            </svg>
-            Gerar
-          </>
-        )}
-      </Button>
-    </form>
+          <Button
+            type="submit"
+            size="lg"
+            aria-label="Gerar GRU"
+            className="mt-3 bg-[#06956A] font-medium hover:bg-[#06945A]"
+            aria-disabled={loading}
+            disabled={loading}
+          >
+            {loading ? (
+              <LoadingIndicator />
+            ) : (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4"
+                >
+                  <polyline points="6 9 6 2 18 2 18 9" />
+                  <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
+                  <rect width="12" height="8" x="6" y="14" />
+                </svg>
+                Gerar
+              </>
+            )}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
