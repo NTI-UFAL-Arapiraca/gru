ffcceda45ced8b023e553a7aa93a545bb019cf8e
Italo Carlo Lopes Silva <italocarlo@nti.ufal.br>
2025-02-20 12:04:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: '/gru',
+    images: {
+      unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
