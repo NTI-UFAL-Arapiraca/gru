747ad639df4c552793032c0a068a1ce0bea6c8c1
Enrique Santos <santosenrique869@gmail.com>
2025-02-20 09:07:11 -0300

    feat(gru-digital): add link to GRU Restaurante Universitário page for Sertão campus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <>
       <section className="grid place-items-center">
-        <div className="relative mb-8 h-40 w-40">
+        <div className="relative mb-6 h-40 w-40">
           <Image
             src="/ufal.png"
             alt="Brasão da UFAL"
@@ -14,19 +14,34 @@ export default function Page() {
           />
         </div>
         <h1 className="text-lg font-bold">Qual serviço deseja usar?</h1>
-        <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
-          <Link
-            href="/gru-ru"
-            className="flex h-20 flex-1 items-center justify-center whitespace-nowrap rounded-md border bg-background px-10  font-medium transition-colors hover:bg-muted"
-          >
-            GRU - RU
-          </Link>
-          <Link
-            href="/gru-sibi"
-            className="flex h-20 flex-1 items-center justify-center whitespace-nowrap rounded-md border bg-background px-10  font-medium transition-colors hover:bg-muted"
-          >
-            GRU - SIBI
-          </Link>
+
+        <div className="mt-4 flex flex-col items-center justify-center">
+          <h2 className="font-semibold">Campus Arapiraca</h2>
+          <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
+            <Link
+              href="/gru-ru"
+              className="flex h-20 flex-1 items-center justify-center whitespace-nowrap rounded-md border bg-background px-10  font-medium transition-colors hover:bg-muted"
+            >
+              GRU - RU
+            </Link>
+            <Link
+              href="/gru-sibi"
+              className="flex h-20 flex-1 items-center justify-center whitespace-nowrap rounded-md border bg-background px-10  font-medium transition-colors hover:bg-muted"
+            >
+              GRU - SIBI
+            </Link>
+          </div>
+        </div>
+        <div className="mt-5 flex flex-col items-center justify-center">
+          <h2 className="font-semibold">Campus Sertão</h2>
+          <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
+            <Link
+              href="/sertao/gru-ru"
+              className="flex h-20 flex-1 items-center justify-center whitespace-nowrap rounded-md border bg-background px-10  font-medium transition-colors hover:bg-muted"
+            >
+              GRU - RU
+            </Link>
+          </div>
         </div>
       </section>
     </>
