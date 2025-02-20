089d891855491dbff9d9618598b2c456dd260725
Enrique Santos <santosenrique869@gmail.com>
2025-02-20 08:44:40 -0300

    feat(gru-digital): add GRU simple data constants for university locations

---
+++ src/constants/gru-simple-data.ts
@@ -0,0 +1,18 @@
+export const GRU_SIMPLE_DATA_BY_UNIVERSITY_LOCATION = {
+  arapiraca: {
+    gruLibrary: {
+      serviceCode: "28830-6",
+      referenceNumber: "1976202",
+    },
+    gruRestaurant: {
+      serviceCode: "28837-3",
+      referenceNumber: "1931002",
+    },
+  },
+  sertao: {
+    gruRestaurant: {
+      serviceCode: "28837-3",
+      referenceNumber: "1931003",
+    },
+  },
+};
